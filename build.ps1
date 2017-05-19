559201a9ef2580df67c56487561e09a0361c99aa
Chris Hunt <chris.hunt@ticketmaster.com>
2017-05-19 15:01:56 -0400

    Add zip creation step to build script

--- build.ps1
+++ build.ps1
@@ -4,3 +4,5 @@ glide up # https://glide.sh/
 
 go test --coverprofile=cover.cov
 go build  -ldflags "-X main.version=$VERSION" -o build/spinner$VERSION.exe
+
+Compress-Archive -Path "build/spinner$VERSION.exe" -Destination "build/spinner$VERSION.zip"
