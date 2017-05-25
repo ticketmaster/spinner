f22e934299c2c91f25a64a73dea048e690b084c5
Chris Hunt <chris.hunt@ticketmaster.com>
2017-05-25 13:31:37 -0400

    Debug create_archives script

--- create_archives.ps1
+++ create_archives.ps1
@@ -1,9 +1,12 @@
 Get-ChildItem .\build\ -Directory | ForEach-Object {Push-Location $_.FullName -StackName build}
 
 while ((Get-Location -Stack -StackName build).count -gt 1) {
+    $pwd
     $osarch = $pwd | Split-Path -leaf
     Compress-Archive .\spinner_v1.0.0* "..\spinner_$osarch-v$env:APPVEYOR_BUILD_VERSION.zip"
     Pop-Location -StackName build
 }
 
 Pop-Location -StackName build
+
+ls .\build
