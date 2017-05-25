a7cf51b2b407eefa4f02274f5d6592066c42fef0
Chris Hunt <chris.hunt@ticketmaster.com>
2017-05-25 14:31:04 -0400

    Fix create_archive script loop

--- create_archives.ps1
+++ create_archives.ps1
@@ -1,6 +1,6 @@
 Get-ChildItem .\build\ -Directory | ForEach-Object {Push-Location $_.FullName -StackName build}
 
-while ((Get-Location -Stack -StackName build).count -gt 1) {
+while ((Get-Location -Stack -StackName build -ErrorAction SilentlyContinue).count -gt 0) {
     $osarch = $pwd | Split-Path -leaf
     Compress-Archive .\spinner* "..\spinner_$osarch-v$env:APPVEYOR_BUILD_VERSION.zip"
     Pop-Location -StackName build
